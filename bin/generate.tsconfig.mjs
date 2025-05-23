4d5de474353fa869507442c5982900af91538a3b
Michał Grabowski <michal.grabowski@ibexa.co>
2025-05-23 14:06:42 +0200

    Added argument to use root project tsconfig (#6)

--- bin/generate.tsconfig.mjs
+++ bin/generate.tsconfig.mjs
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { globSync } from 'glob';
 
+const useRootProjectTSConfig = process.argv.includes('--use-root-project-tsconfig') || process.env.USE_ROOT_PROJECT_TSCONFIG === 'true';
 const configSetupsAggregatorFilePath = './var/encore/ibexa.config.setup.js';
 const configSetupsAggregatorFullFilePath = path.resolve(configSetupsAggregatorFilePath);
 const customConfigFilePath = './custom.tsconfig.mjs';
@@ -56,10 +57,15 @@ const saveConfigFileContent = (fileName, content) => {
     const configFilePath = path.resolve(fileName);
     const contentJSON = JSON.stringify(content, null, 4);
 
-    fs.writeFileSync(configFilePath, contentJSON);
+    fs.writeFileSync(configFilePath, `${contentJSON}\n`, 'utf-8');
 };
 const sortConfigAliases = (configContent) => {
-    const { paths } = configContent.compilerOptions;
+    const { paths } = configContent?.compilerOptions ?? {};
+
+    if (!paths) {
+        return;
+    }
+
     const pathsSorted = Object.keys(paths)
         .toSorted()
         .reduce(
@@ -81,7 +87,12 @@ const updatePathsConfig = (config, aliases) => {
         ...(config.compilerOptions?.paths ?? {}),
     };
 
-    config.compilerOptions.paths = paths;
+    if (Object.keys(paths).length) {
+        config.compilerOptions = {
+            ...config.compilerOptions ?? {},
+            paths,
+        };
+    }
 };
 const updateCustomConfig = async (configContent) => {
     if (fs.existsSync(customConfigFilePath)) {
@@ -92,12 +103,44 @@ const updateCustomConfig = async (configContent) => {
 
     return configContent;
 };
-const encoreSetupMethods = await getEncoreAliasSetupMethods();
-let ibexaConfigContent = getConfigFileContent('ibexa.tsconfig.json');
-const encoreAliasesList = getEncoreAliases(encoreSetupMethods);
-const aliasesListMerged = mergeAliases(encoreAliasesList, ibexaConfigContent.compilerOptions?.paths ?? {});
+const findRootProjectTSConfigPath = () => {
+    if (!useRootProjectTSConfig) {
+        return;
+    }
 
-updatePathsConfig(ibexaConfigContent, aliasesListMerged);
+    let currentDir = process.cwd();
+
+    while (currentDir !== '/') {
+        const webpackConfigPath = path.join(currentDir, 'ibexa.webpack.config.js');
+        const tsConfigPath = path.join(currentDir, 'tsconfig.json');
+
+        if (fs.existsSync(webpackConfigPath) && fs.existsSync(tsConfigPath)) {
+            return tsConfigPath;
+        }
+
+        currentDir = path.dirname(currentDir);
+    }
+
+    return;
+};
+const updateParentConfig = (configContent) => {
+    if (rootProjectTSConfigPath) {
+        configContent.extends = rootProjectTSConfigPath;
+    }
+};
+let ibexaConfigContent = getConfigFileContent('ibexa.tsconfig.json');
+const rootProjectTSConfigPath = findRootProjectTSConfigPath();
+
+updateParentConfig(ibexaConfigContent, rootProjectTSConfigPath);
+
+if (!rootProjectTSConfigPath) {
+    const encoreSetupMethods = await getEncoreAliasSetupMethods();
+    const encoreAliasesList = getEncoreAliases(encoreSetupMethods);
+    const aliasesListMerged = mergeAliases(encoreAliasesList, ibexaConfigContent.compilerOptions?.paths ?? {});
+
+    updatePathsConfig(ibexaConfigContent, aliasesListMerged);
+}
+
 sortConfigAliases(ibexaConfigContent);
 
 ibexaConfigContent = await updateCustomConfig(ibexaConfigContent);
