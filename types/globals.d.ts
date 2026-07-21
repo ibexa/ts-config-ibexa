df1b259d8fe86cdad90169dcc651fe0ba51ad966
Michał Grabowski <michal.grabowski@ibexa.co>
2026-07-21 13:32:03 +0200

    fixed types

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,13 +1,16 @@
 import type { Translator } from './translator';
 
 declare global {
+    interface Ibexa {
+        addConfig(path: string, value: unknown, merge?: boolean): void;
+    }
+
     interface Window {
         bootstrap: typeof import('bootstrap');
         Chart: typeof import('chart.js');
         ChartDataLabels: typeof import('chartjs-plugin-datalabels');
         flatpickr: typeof import('flatpickr');
-        ibexa: any;
-        ibexaCart: any;
+        ibexa: Ibexa;
         L: typeof import('leaflet');
         moment: typeof import('moment');
         Popper: typeof import('@popperjs/core');
